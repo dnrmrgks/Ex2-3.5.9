fdbeaf2ec530921215014311bae0b39a438cf370
park sung chan <dnrmrgks@naver.com>
2014-01-13 17:04:43 +0900

    Initial Commit

---
+++ Ex2-3.5.9/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex2-3.5.9
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex2-3.5.9/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ex2-3.5.9
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
