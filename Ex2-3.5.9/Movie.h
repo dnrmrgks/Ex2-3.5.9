364b7cce18a62daf3a9f8f4e05889141d130557c
park sung chan <dnrmrgks@naver.com>
2014-01-14 10:52:10 +0900

    2-3.5.9
    db

---
+++ Ex2-3.5.9/Movie.h
@@ -0,0 +1,16 @@
+//
+//  Movie.h
+//  Ex2-3.5.9
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+
+@property(copy,nonatomic)NSString *title;
+@property int rowID;
+
+@end
---
+++ Ex2-3.5.9/Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  Ex2-3.5.9
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+@end
